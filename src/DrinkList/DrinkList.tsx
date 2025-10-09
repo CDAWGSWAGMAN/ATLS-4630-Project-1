05cad8fca99e58ef8d9deba4e6e4b5e5c66eaaf2
cooperjones13 <39482154+cooperjones13@users.noreply.github.com>
2025-10-09 08:38:36 -0600

    Made Drink List Components

---
+++ src/DrinkList/DrinkList.tsx
@@ -0,0 +1,19 @@
+import type { Drink } from "../App/App"
+import "./DrinkList.css"
+import ListItem from "./ListItem/ListItem"
+
+type DrinkListProps = {
+    drinkResultList: Drink[]
+}
+
+export default function DrinkList({drinkResultList}: DrinkListProps){
+    return(
+        <div className="DrinkListContainer">
+            {
+                drinkResultList.map(drink =>
+                    <ListItem drink={drink}/>
+                )
+            }
+        </div>
+    )
+}
